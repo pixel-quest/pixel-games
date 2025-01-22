7da19f112d63c0a8aa22cc65aa0e07b7ecbe8f77
Roman Avondale <yandproma@yandex.com>
2025-01-22 08:35:30 +0300

    heroes lib + color

--- games/heroes_v1/heroes_v1.lua
+++ games/heroes_v1/heroes_v1.lua
@@ -19,6 +19,7 @@
 
 ]]
 math.randomseed(os.time())
+require("avonlib")
 
 local CLog = require("log")
 local CInspect = require("inspect")
@@ -189,12 +190,12 @@ CPaint.PaintBG = function()
 
             local tBackgroundColor = CPaint.GetBackgroundColor(iX, iY)
             if tBackgroundColor ~= nil then
-                tFloor[iX][iY].iColor = tBackgroundColor.color
+                tFloor[iX][iY].iColor = tonumber(tBackgroundColor.color)
                 tFloor[iX][iY].iBright = tBackgroundColor.bright
             end
 
-            if tBg ~= nil and tBg[iBGY][iBGX] ~= 8 then
-                tFloor[iX][iY].iColor = tBg[iBGY][iBGX]
+            if tBg ~= nil and tBg[iBGY][iBGX] ~= "0xEMPTY1" then
+                tFloor[iX][iY].iColor = tonumber(tBg[iBGY][iBGX])
                 tFloor[iX][iY].iBright = tConfig.Bright
 
                 if tBgBr ~= nil and tBgBr[iBGY][iBGX] ~= 9 then
@@ -239,73 +240,6 @@ CPaint.GetBackgroundColor = function(iX, iY)
     return tGame.Colors[iId]
 end
 --//
-
----------------AVONLIB TEST
-_G.AL = {}
-local LOC = {}
-
---STACK
-AL.Stack = function()
-    local tStack = {}
-    tStack.tTable = {}
-
-    tStack.Push = function(item)
-        table.insert(tStack.tTable, item)
-    end
-
-    tStack.Pop = function()
-        return table.remove(tStack.tTable, 1)
-    end
-
-    tStack.Size = function()
-        return #tStack.tTable
-    end
-
-    return tStack
-end
---//
-
---TIMER
-local tTimers = AL.Stack()
-
-AL.NewTimer = function(iSetTime, fCallback)
-    tTimers.Push({iTime = iSetTime, fCallback = fCallback})
-end
-
-AL.CountTimers = function(iTimePassed)
-    for i = 1, tTimers.Size() do
-        local tTimer = tTimers.Pop()
-
-        tTimer.iTime = tTimer.iTime - iTimePassed
-
-        if tTimer.iTime <= 0 then
-            local iNewTime = tTimer.fCallback()
-            if iNewTime then
-                tTimer.iTime = tTimer.iTime + iNewTime
-            else
-                tTimer = nil
-            end
-        end
-
-        if tTimer then
-            tTimers.Push(tTimer)
-        end
-    end
-end
---//
-
---RECT
-function AL.RectIntersects(iX1, iY1, iSize1, iX2, iY2, iSize2)
-    if iSize1 == 0 or iSize2 == 0 then return false; end
-
-    if iX1 > iX2+iSize2-1 or iX2 > iX1+iSize1-1 then return false; end
-
-    if iY1+iSize1-1 < iY2 or iY2+iSize2-1 < iY1 then return false; end
-
-    return true
-end
---//
----------------//
 
 --UTIL прочие утилиты
 function SetGlobalColorBright(iColor, iBright)
