c0411aeb5aaac52d6de488fbd19e7959ff853628
Roman Avondale <yandproma@yandex.com>
2025-03-13 16:49:45 +0300

    Update water_circles_v1.lua

--- games/water_circles_v1/water_circles_v1.lua
+++ games/water_circles_v1/water_circles_v1.lua
@@ -265,6 +265,11 @@ end
 
 function PixelClick(click)
     if tFloor[click.X] and tFloor[click.X][click.Y] then
+        if bGamePaused then
+            tFloor[click.X][click.Y].bClick = false
+            return;
+        end
+        
         tFloor[click.X][click.Y].bClick = click.Click
         tFloor[click.X][click.Y].iWeight = click.Weight
 
