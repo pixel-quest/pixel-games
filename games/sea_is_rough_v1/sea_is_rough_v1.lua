eda0b04a42c4dcef53965a8a4a1657de2ebe8e1f
Roman Avondale <yandproma@yandex.com>
2025-03-17 14:44:33 +0300

    sea auto fill start

--- games/sea_is_rough_v1/sea_is_rough_v1.lua
+++ games/sea_is_rough_v1/sea_is_rough_v1.lua
@@ -64,14 +64,6 @@ local GameObj = {
     Rows = 15, -- пикселей по вертикали (Y), обязательные параметр для всех игр
     Buttons = {2, 6, 10, 14, 18, 22, 26, 30, 34, 42, 46, 50, 54, 58, 62, 65, 69, 73, 77}, -- номера кнопок в комнате
     StartPositionSize = 2, -- размер стартовой зоны для игрока, для маленькой выездной платформы удобно ставить тут 1
-    StartPositions = { -- координаты расположения стартовых зон должны быть возле стены, т.к для старта надо нажать кнопку на стене
-        { X = 2, Y = 2, Color = colors.RED },
-        { X = 6, Y = 2, Color = colors.YELLOW },
-        { X = 10, Y = 2, Color = colors.GREEN },
-        { X = 14, Y = 2, Color = colors.CYAN },
-        { X = 18, Y = 2, Color = colors.BLUE },
-        { X = 22, Y = 2, Color = colors.MAGENTA },
-    },
     BurnEffect = { -- тайминги эффекта моргания
         DurationOnMs = 200,
         DurationOffMs = 120,
@@ -186,9 +178,28 @@ function StartGame(gameJson, gameConfigJson) -- старт игры
         end
     end
 
-    for iPlayerID = 1, #GameObj.StartPositions do
-        GameObj.StartPositions[iPlayerID].Color = tonumber(GameObj.StartPositions[iPlayerID].Color)
-    end    
+    if GameObj.StartPositions == nil then
+        GameObj.StartPositions = {}
+
+        local iX = 2
+        local iY = math.floor(GameObj.Rows/2)
+        for iPlayerID = 1, 6 do
+            GameObj.StartPositions[iPlayerID] = {}
+            GameObj.StartPositions[iPlayerID].X = iX
+            GameObj.StartPositions[iPlayerID].Y = iY
+            GameObj.StartPositions[iPlayerID].Color = tColors[iPlayerID]
+
+            iX = iX + (GameObj.StartPositionSize*2)
+            if iX + GameObj.StartPositionSize > GameObj.Cols then
+                iX = 2
+                iY = iY + (GameObj.StartPositionSize+1)
+            end
+        end
+    else
+        for iPlayerID = 1, #GameObj.StartPositions do
+            GameObj.StartPositions[iPlayerID].Color = tonumber(GameObj.StartPositions[iPlayerID].Color)
+        end 
+    end     
 
     for i, num in pairs(GameObj.Buttons) do
         ButtonsList[num] = help.ShallowCopy(Pixel) -- тип аналогичен пикселю
