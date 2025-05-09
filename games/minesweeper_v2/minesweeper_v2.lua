8361b68e4d42dbf12ef787ece6e1b6b07f335fe5
Roman Avondale <yandproma@yandex.com>
2025-05-09 13:21:25 +0300

    minesweeper2 autostart update

--- games/minesweeper_v2/minesweeper_v2.lua
+++ games/minesweeper_v2/minesweeper_v2.lua
@@ -156,13 +156,13 @@ function GameSetupTick()
         CPaint.PlayerZone(iPlayerID, iBright)           
     end
 
-    if (iPlayersReady > 1 and bAnyButtonClick) or (iPlayersReady >= tConfig.AutoStartPlayerCount) then
+    if (iPlayersReady > 1 and bAnyButtonClick) or (iPlayersReady >= #tGame.StartPositions) then
         bAnyButtonClick = false
         CGameMode.iAlivePlayerCount = iPlayersReady
         iGameState = GAMESTATE_GAME
 
         CAudio.PlayVoicesSync("minesweeper/minesweeper-guide.mp3")
-        CGameMode.StartNextRoundCountDown(20)
+        CGameMode.StartNextRoundCountDown(1 + CAudio.GetVoicesDuration("minesweeper/minesweeper-guide.mp3"))
     end
 end
 
