a1e16762c4dbe95c5d3eb42d3e68a5e26ebd9f44
Roman Avondale <yandproma@yandex.com>
2025-05-28 18:13:29 +0300

    avonlib WallZone + test

--- games/pirates_v1/pirates_v1.lua
+++ games/pirates_v1/pirates_v1.lua
@@ -107,15 +107,31 @@ function StartGame(gameJson, gameConfigJson)
 
     iPrevTickTime = CTime.unix()
 
+    if AL.RoomHasNFZ() then
+        AL.LoadNFZInfo()
+    end
+
     if tGame.StartPositions == nil then
         tGame.StartPositions = {}
 
-        tGame.StartPositionSizeX = math.floor(tGame.Cols/tConfig.PlayerCount)-1
+        local iMaxX = tGame.Cols-1
+        local iMaxY = tGame.Rows
+        if AL.NFZ.bLoaded then
+            iMaxX = AL.NFZ.iMaxX
+            iMaxY = AL.NFZ.iMaxY
+        end
+
+        tGame.StartPositionSizeX = math.floor(iMaxX/tConfig.PlayerCount)-1
         if tGame.StartPositionSizeX < 4 then tGame.StartPositionSizeX = 4; end
-        tGame.StartPositionSizeY = tGame.Rows
+        tGame.StartPositionSizeY = iMaxY
 
         local iX = 0
         local iY = 1
+
+        if AL.NFZ.bLoaded then
+            iX = AL.NFZ.iMinX
+            iY = AL.NFZ.iMinY
+        end
 
         for iPlayerID = 1, tConfig.PlayerCount do
             tGame.StartPositions[iPlayerID] = {}
@@ -124,7 +140,7 @@ function StartGame(gameJson, gameConfigJson)
             tGame.StartPositions[iPlayerID].Color = tTeamColors[iPlayerID]
 
             iX = iX + tGame.StartPositionSizeX + 1
-            if iX + tGame.StartPositionSizeX-1 > tGame.Cols then
+            if iX + tGame.StartPositionSizeX-1 > iMaxX then
                 break;
             end
         end
@@ -679,3 +695,124 @@ function DefectButton(defect)
         tButtons[defect.Button].iBright = CColors.BRIGHT0
     end    
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+--RECT
+function AL.RectIntersects(iX1, iY1, iSize1, iX2, iY2, iSize2)
+    if iSize1 == 0 or iSize2 == 0 then return false; end
+
+    if iX1 > iX2+iSize2-1 or iX2 > iX1+iSize1-1 then return false; end
+
+    if iY1+iSize1-1 < iY2 or iY2+iSize2-1 < iY1 then return false; end
+
+    return true
+end
+
+function AL.RectIntersects2(iX1, iY1, iSizeX1, iSizeY1, iX2, iY2, iSizeX2, iSizeY2)
+    if iSize1 == 0 or iSize2 == 0 then return false; end
+
+    if iX1 > iX2+iSizeX2-1 or iX2 > iX1+iSizeX1-1 then return false; end
+
+    if iY1+iSizeY1-1 < iY2 or iY2+iSizeY2-1 < iY1 then return false; end
+
+    return true
+end
+--//
+
+--NFZ - NoFeetZone / Pixel Wall Zone / Зона Пиксельной Стены
+AL.NFZ = {}
+
+AL.RoomHasNFZ = function()
+    if tGame and tGame.NoFeetZones ~= nil then
+        if #tGame.NoFeetZones > 0 then 
+            return true; 
+        end
+    end
+
+    return false;
+end
+
+AL.IsPixelInNFZ = function(iX, iY)
+    return AL.IsRectInNFZ(iX, iY, 1, 1)
+end
+
+AL.IsRectInNFZ = function(iX, iY, iSizeX, iSizeY)
+    for iNFZId = 1, #tGame.NoFeetZones do
+        if AL.RectIntersects2(iX, iY, iSizeX, iSizeY, tGame.NoFeetZones[iNFZId].X,tGame.NoFeetZones[iNFZId].Y, tGame.NoFeetZones[iNFZId].SizeX, tGame.NoFeetZones[iNFZId].SizeY) then
+            return true;
+        end
+    end
+
+    return false    
+end
+
+AL.LoadNFZInfo = function()
+    AL.NFZ = {}
+
+    AL.NFZ.iMinX = NFZ_LoadMinX()
+    AL.NFZ.iMinY = NFZ_LoadMinY()
+    AL.NFZ.iMaxX = NFZ_LoadMaxX()
+    AL.NFZ.iMaxY = NFZ_LoadMaxY()   
+
+    AL.NFZ.iCenterX = math.floor((AL.NFZ.iMaxX - AL.NFZ.iMinX)/2)
+    AL.NFZ.iCenterY = math.floor((AL.NFZ.iMaxY - AL.NFZ.iMinY)/2)
+
+    AL.NFZ.bLoaded = true
+end
+
+NFZ_LoadMinX = function()
+    local iMinX = 1
+
+    for iNFZId = 1, #tGame.NoFeetZones do
+        if tGame.NoFeetZones[iNFZId].X + tGame.NoFeetZones[iNFZId].SizeX < math.floor(tGame.Cols/2) and (tGame.NoFeetZones[iNFZId].X + tGame.NoFeetZones[iNFZId].SizeX) > iMinX then
+            iMinX = tGame.NoFeetZones[iNFZId].X + tGame.NoFeetZones[iNFZId].SizeX
+        end
+    end    
+
+    return iMinX
+end
+NFZ_LoadMinY = function()
+    local iMinY = 1
+
+    for iNFZId = 1, #tGame.NoFeetZones do
+        if tGame.NoFeetZones[iNFZId].Y + tGame.NoFeetZones[iNFZId].SizeY-1 < math.floor(tGame.Rows/2) and (tGame.NoFeetZones[iNFZId].Y + tGame.NoFeetZones[iNFZId].SizeY-1) > iMinY then
+            iMinY = tGame.NoFeetZones[iNFZId].Y + tGame.NoFeetZones[iNFZId].SizeY-1
+        end
+    end    
+
+    return iMinY
+end
+NFZ_LoadMaxX = function()
+    local iMaxX = tGame.Cols
+
+    for iNFZId = 1, #tGame.NoFeetZones do
+        if tGame.NoFeetZones[iNFZId].SizeX < tGame.Cols/2 and tGame.NoFeetZones[iNFZId].X > math.floor(tGame.Cols/2) and tGame.NoFeetZones[iNFZId].X < iMaxX then
+            iMaxX = tGame.NoFeetZones[iNFZId].X
+        end
+    end    
+
+    return iMaxX
+end
+NFZ_LoadMaxY = function()
+    local iMaxY = tGame.Rows
+
+    for iNFZId = 1, #tGame.NoFeetZones do
+        if tGame.NoFeetZones[iNFZId].SizeY < tGame.Rows/2 and tGame.NoFeetZones[iNFZId].Y > math.floor(tGame.Rows/2) and tGame.NoFeetZones[iNFZId].Y < iMaxY then
+            iMaxY = tGame.NoFeetZones[iNFZId].Y
+        end
+    end    
+
+    return iMaxY
+end
+--//
