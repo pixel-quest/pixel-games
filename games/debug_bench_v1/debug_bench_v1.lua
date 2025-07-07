3fc5be05813bdc0b760e5f07430b29e590580e2b
Roman Avondale <yandproma@yandex.com>
2025-07-07 11:40:48 +0300

    debug bench script

---
+++ games/debug_bench_v1/debug_bench_v1.lua
@@ -0,0 +1,268 @@
+--[[
+    Название: Название механики
+    Автор: Avondale, дискорд - avonda
+    Описание механики: в общих словах, что происходит в механике
+    Идеи по доработке: то, что может улучшить игру, но не было реализовано здесь
+]]
+math.randomseed(os.time())
+require("avonlib")
+
+local CLog = require("log")
+local CInspect = require("inspect")
+local CHelp = require("help")
+local CJson = require("json")
+local CTime = require("time")
+local CAudio = require("audio")
+local CColors = require("colors")
+
+local tGame = {
+    Cols = 24,
+    Rows = 15, 
+    Buttons = {}, 
+}
+local tConfig = {}
+
+local bGamePaused = false
+local iPrevTickTime = 0
+
+local tGameStats = {
+    StageLeftDuration = 0, 
+    StageTotalDuration = 0, 
+    CurrentStars = 0,
+    TotalStars = 0,
+    CurrentLives = 0,
+    TotalLives = 0,
+    Players = { -- максимум 6 игроков
+        { Score = 0, Lives = 0, Color = CColors.NONE },
+        { Score = 0, Lives = 0, Color = CColors.NONE },
+        { Score = 0, Lives = 0, Color = CColors.NONE },
+        { Score = 0, Lives = 0, Color = CColors.NONE },
+        { Score = 0, Lives = 0, Color = CColors.NONE },
+        { Score = 0, Lives = 0, Color = CColors.NONE },
+    },
+    TargetScore = 0,
+    StageNum = 1,
+    TotalStages = 2,
+    TargetColor = CColors.NONE,
+    ScoreboardVariant = 0,
+}
+
+local tGameResults = {
+    Won = false,
+    AfterDelay = false,
+    PlayersCount = 0,
+    Score = 0,
+    Color = CColors.NONE,
+}
+
+local tFloor = {} 
+local tButtons = {}
+
+local tFloorStruct = { 
+    iColor = CColors.NONE,
+    iBright = CColors.BRIGHT0,
+    bClick = false,
+    bDefect = false,
+    iWeight = 0,
+}
+local tButtonStruct = { 
+    bClick = false,
+    bDefect = false,
+}
+
+local tColors = {}
+tColors[1] = CColors.GREEN
+tColors[2] = CColors.RED
+tColors[3] = CColors.YELLOW
+tColors[4] = CColors.MAGENTA
+tColors[5] = CColors.CYAN
+tColors[6] = CColors.BLUE
+tColors[7] = CColors.WHITE
+
+local iCurrentColor = tColors[math.random(1,#tColors)]
+
+function StartGame(gameJson, gameConfigJson)
+    tGame = CJson.decode(gameJson)
+    tConfig = CJson.decode(gameConfigJson)
+
+    for iX = 1, tGame.Cols do
+        tFloor[iX] = {}    
+        for iY = 1, tGame.Rows do
+            tFloor[iX][iY] = CHelp.ShallowCopy(tFloorStruct) 
+        end
+    end
+
+    for _, iId in pairs(tGame.Buttons) do
+        tButtons[iId] = CHelp.ShallowCopy(tButtonStruct)
+    end
+
+    iPrevTickTime = CTime.unix() 
+
+    AL.NewTimer(5000, function()
+        if tGameStats.StageNum == 1 then
+            iCurrentColor = tColors[math.random(1,#tColors)]
+            return 5000
+        else
+            return nil
+        end
+    end)
+end
+
+function NextTick()
+    if iCurrentColor ~= CColors.NONE then
+        SetGlobalColorBright(iCurrentColor, tConfig.Bright)
+    end
+
+    if tGameStats.StageNum > tGameStats.TotalStages then
+        tGameResults.AfterDelay = false
+        return tGameResults
+    end     
+
+    AL.CountTimers((CTime.unix() - iPrevTickTime) * 1000)
+    iPrevTickTime = CTime.unix()
+end
+
+function RangeFloor(setPixel, setButton)
+    for iX = 1, tGame.Cols do
+        for iY = 1, tGame.Rows do
+            setPixel(iX , iY, tFloor[iX][iY].iColor, tFloor[iX][iY].iBright)
+        end
+    end
+
+    for i, tButton in pairs(tButtons) do
+        setButton(i, tButton.iColor, tButton.iBright)
+    end
+end
+
+function SwitchStage()
+    tGameStats.StageNum = tGameStats.StageNum + 1
+    SetGlobalColorBright(CColors.NONE, tConfig.Bright)
+end
+
+--UTIL прочие утилиты
+function CheckPositionClick(tStart, iSizeX, iSizeY)
+    for iX = tStart.X, tStart.X + iSizeX - 1 do
+        for iY = tStart.Y, tStart.Y + iSizeY - 1 do
+            if tFloor[iX] and tFloor[iX][iY] then
+                if tFloor[iX][iY].bClick then
+                    return true
+                end 
+            end
+        end
+    end
+
+    return false
+end
+
+function SetPositionColorBright(tStart, iSize, iColor, iBright)
+    for i = 0, iSize * iSize - 1 do
+        local iX = tStart.X + i % iSize
+        local iY = tStart.Y + math.floor(i / iSize)
+
+        if not (iX < 1 or iX > tGame.Cols or iY < 1 or iY > tGame.Rows) then     
+            tFloor[iX][iY].iColor = iColor
+            tFloor[iX][iY].iBright = iBright            
+        end
+    end
+end
+
+function SetRectColorBright(iX, iY, iSizeX, iSizeY, iColor, iBright)
+    for i = iX, iX + iSizeX do
+        for j = iY, iY + iSizeY do
+            if not (i < 1 or i > tGame.Cols or j < 1 or j > tGame.Rows) and not tFloor[i][j].bAnimated then     
+                tFloor[i][j].iColor = iColor
+                tFloor[i][j].iBright = iBright            
+            end            
+        end
+    end
+end
+
+function SetGlobalColorBright(iColor, iBright)
+    for iX = 1, tGame.Cols do
+        for iY = 1, tGame.Rows do
+            tFloor[iX][iY].iColor = iColor
+            tFloor[iX][iY].iBright = iBright
+        end
+    end
+
+    for i, tButton in pairs(tButtons) do
+        tButtons[i].iColor = iColor
+        tButtons[i].iBright = iBright
+    end
+end
+
+function SetAllButtonColorBright(iColor, iBright, bCheckDefect)
+    for i, tButton in pairs(tButtons) do
+        if not bCheckDefect or not tButtons[i].bDefect then
+            tButtons[i].iColor = iColor
+            tButtons[i].iBright = iBright
+        end
+    end
+end
+--//
+
+
+--//
+function GetStats()
+    return tGameStats
+end
+
+function PauseGame()
+    bGamePaused = true
+end
+
+function ResumeGame()
+    bGamePaused = false
+	iPrevTickTime = CTime.unix()
+end
+
+function PixelClick(click)
+    if tFloor[click.X] and tFloor[click.X][click.Y] then
+        if bGamePaused then
+            tFloor[click.X][click.Y].bClick = false
+            return;
+        end
+
+        if tGameStats.StageNum == 2 and tFloor[click.X][click.Y].iColor == CColors.NONE then
+            tFloor[click.X][click.Y].iBright = 7
+            AL.NewTimer(0, function()
+                tFloor[click.X][click.Y].iColor = tColors[math.random(1,#tColors)]
+                tFloor[click.X][click.Y].iBright = tFloor[click.X][click.Y].iBright - 1
+                if tFloor[click.X][click.Y].iBright > 0 then
+                    return 250
+                end
+                return nil
+            end)
+        end 
+
+        tFloor[click.X][click.Y].bClick = click.Click
+        tFloor[click.X][click.Y].iWeight = click.Weight
+    end
+end
+
+function DefectPixel(defect)
+    if tFloor[defect.X] and tFloor[defect.X][defect.Y] then
+        tFloor[defect.X][defect.Y].bDefect = defect.Defect
+    end
+end
+
+function ButtonClick(click)
+    if tButtons[click.Button] == nil or bGamePaused or tButtons[click.Button].bDefect then return end
+    tButtons[click.Button].bClick = click.Click
+
+    if click.Click then 
+        iCurrentColor = CColors.NONE
+        SetGlobalColorBright(CColors.NONE, tConfig.Bright)
+        tGameStats.StageNum = 2
+    end
+end
+
+function DefectButton(defect)
+    if tButtons[defect.Button] == nil then return end
+    tButtons[defect.Button].bDefect = defect.Defect
+
+    if defect.Defect then
+        tButtons[defect.Button].iColor = CColors.NONE
+        tButtons[defect.Button].iBright = CColors.BRIGHT0
+    end    
+end
